81c37bde6227d13d8e5e25d36e75e64afe793c95
d3diva <d3diva@gmail.com>
2016-05-25 04:17:36 -0400

    list print

--- todolist.rb
+++ todolist.rb
@@ -47,8 +47,78 @@ class TodoList
         puts "#{idescription} #{message}"
         print_line
       end
+
+      # Prints header
+      def print_header
+        "#{"Sl."} #{"Description".ljust(20)} #{"Created time".ljust(10)}  #{"Due by".ljust(10)} #{"Status"}"
+      end
+
+      # Prints title
+      def print_title
+        "#{''.ljust(20)} #{title}"
+      end
+
+      # Prints Prepared date
+      def print_date
+        "#{''.ljust(15)} Prepared Date: #{@tdate}"
+      end
+
+      # Prints report to file
+      def report_print_items
+        $report_file.puts print_line
+        $report_file.puts print_date
+        $report_file.puts print_title
+        $report_file.puts print_line
+        $report_file.puts print_header
+        $report_file.puts print_line
+        items.each_with_index do |item, index_no|
+          $report_file.puts item.print_item_details(index_no)
+        end
+        $report_file.puts print_line
+      end
+
+      # Out put list to terminal
+      def print_items
+        puts print_line
+        puts print_date
+        puts print_title
+        puts print_line
+        puts print_header
+        puts print_line
+        items.each_with_index do |item, index_no|
+          puts item.print_item_details(index_no)
+        end
+        puts print_line
+      end
+
+      def print_line
+           "-" * 60
+      end
 end
 
 class Item
     # methods and stuff go here
+    attr_accessor :description, :completed_status, :created_time, :due_time
+
+    # Initialize item with a description and marked as
+    # not complete
+    def initialize(item_description)
+       @description = item_description
+       @completed_status = false
+       @created_time = Time.now.strftime("%H:%M")
+       @due_time = (Time.now + 18000).strftime("%H:%M")
+    end
+
+    # prints items details
+    def print_item_details(index_no)
+      "#{index_no +1} #{@description.ljust(25)} #{@created_time.ljust(10)} #{@due_time.ljust(12)} #{task_complete?? "\u2611" : "\u2610"}"
+    end
+
+    # verify status
+    def task_complete?
+      @completed_status
+    end
+
+
+
 end
